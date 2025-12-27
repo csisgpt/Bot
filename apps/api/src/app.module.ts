7f2200485a035af6c09d9cb85783330bcf8edbb8
csisgpt <csisgpt@gmail.com>
2025-12-27 16:47:37 +0330

    Import ScheduleModule in app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { ConfigService } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { CoreModule, createRedisConnection, SIGNALS_QUEUE_NAME } from '@libs/core';
 import { TelegramModule } from '@libs/telegram';
 import { AdminController } from './admin.controller';
