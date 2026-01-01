70467a62322e64813ed87f24fe856b1f75588d98
csisgpt <csisgpt@gmail.com>
2026-01-01 13:51:53 +0330

    Update feeds.module.ts

--- apps/worker/src/feeds/feeds.module.ts
+++ apps/worker/src/feeds/feeds.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CoreModule } from '@libs/core';
-import { FeedRunnerService } from './feed-runner.service';
-import { NewsModule } from '../news/news.module';
-import { TelegramPublisherModule } from '../telegram/telegram-publisher.module';
-import { MarketDataModule } from '@libs/market-data';
-import { MarketDataV3Module } from '../market-data-v3/market-data-v3.module';
 import { FeedConfigService } from './feed-config.service';
+import { FeedRunnerService } from './feed-runner.service';
+import { FeedsSchedulerService } from './feeds.scheduler';
 
 @Module({
-  imports: [CoreModule, NewsModule, MarketDataModule, MarketDataV3Module, TelegramPublisherModule],
-  providers: [FeedRunnerService, FeedConfigService],
+  providers: [FeedConfigService, FeedRunnerService, FeedsSchedulerService],
 })
 export class FeedsModule {}
